9126ca4666b1823fcdf84c17f04a585b7da1e2ad
Dratyti <belousovi783@gmail.com>
2024-02-22 17:43:33 +0300

    приложенин main готово, index и about

--- main/views.py
+++ main/views.py
@@ -2,13 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request): #представления или контролерр
+def index(request):
     context = {
-        'title': 'Home',
-        'context': "главная страница- HOME"
+        'title': 'Home - Главная',
+        'content': "Магазин мебели HOME",
     }
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home - О нас',
+        'content': "О нас",
+        'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар."
+    }
+
+    return render(request, 'main/about.html', context)
