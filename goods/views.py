c64b8f13a073fb456d9701f0885b100c5836df61
Dratyti <belousovi783@gmail.com>
2024-02-24 15:29:08 +0300

    создание своего tag для categories

--- goods/views.py
+++ goods/views.py
@@ -1,27 +1,15 @@
 from django.shortcuts import render
 
+from goods.models import Products
+
 # Create your views here.
 
 
 def catalog(request):
+    goods = Products.objects.all()
     context = {
         'title': "Home - Catalog",
-        'goods': [
-            {'image': 'deps/images/goods/set of tea table and three chairs.jpg',
-             'name': 'Чайный столик и три стула',
-             'description': 'Комплект из трёх стульев и дизайнерский столик для гостинной комнаты.',
-             'price': 150.00},
-
-            {'image': 'deps/images/goods/set of tea table and two chairs.jpg',
-             'name': 'Чайный столик и два стула',
-             'description': 'Набор из стола и двух стульев в минималистическом стиле.',
-             'price': 93.00},
-
-            {'image': 'deps/images/goods/double bed.jpg',
-                'name': 'Двухспальная кровать',
-                'description': 'Кровать двухспальная с надголовником и вообще очень ортопедичная.',
-                'price': 670.00}
-        ]
+        'goods': goods,
     }
     return render(request, "goods/catalog.html", context)
 
