d1f99e0f1be5b5236d770f3d47215e9238ff3342
Dratyti <belousovi783@gmail.com>
2024-03-09 15:23:50 +0300

    покупка товара

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from carts.admin import CartTabAdmin
+from orders.admin import OrderTabulareAdmin
 
 
 from .models import User
@@ -13,4 +14,4 @@ class CategoriesAdmin(admin.ModelAdmin):
     list_display = ['username', 'is_superuser']
     search_fields = ["username", "last_name", 'first_name']
 
-    inlines = [CartTabAdmin]
+    inlines = [CartTabAdmin, OrderTabulareAdmin,]
